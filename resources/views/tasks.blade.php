331bf34b19c59483770aca8da32c53c71361c0f1
gribeiro207 <126004015+gribeiro207@users.noreply.github.com>
2023-03-30 14:39:05 +0100

    first commit

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -41,7 +41,7 @@
             @if (count($tasks) > 0)
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Current Tasks
+                        Current Tasks - modified
                     </div>
 
                     <div class="panel-body">
