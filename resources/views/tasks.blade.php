44403390b66b0306bc71a8d555c2d734bb7a0556
gribeiro207 <126004015+gribeiro207@users.noreply.github.com>
2023-03-30 14:43:20 +0100

    second commit

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -41,7 +41,7 @@
             @if (count($tasks) > 0)
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Current Tasks - modified
+                        Current Tasks - modified 2
                     </div>
 
                     <div class="panel-body">
